1a5ccd7b1557301e548196bb8102b5a46e0c3425
Henrique-BO <henrique.barrosoliveira@usp.br>
2022-06-17 12:05:38 -0300

    Comandos G0 e G1 no Controlador

--- include/controlador.h
+++ include/controlador.h
@@ -5,6 +5,9 @@
 // #include "definicoes_sistema.h"
 
 #define MAX_SPEED 100.0
+#define MM_POR_STEP_X 1.0
+#define MM_POR_STEP_Y 1.0
+#define MM_POR_STEP_Z 1.0
 #define CONTROLADOR_DELAY 10 // TODO calcular a frequencia necessaria
 
 void vTaskControlador(void *param);
@@ -25,6 +28,8 @@ class Controlador {
         AccelStepper *pStepperX;
         AccelStepper *pStepperY;
         AccelStepper *pStepperZ;
+        float speed = 100.0;
+        bool calibrando = false;
 };
 
 #endif
--- src/controlador.cpp
+++ src/controlador.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 #include "AccelStepper.h"
+#include <cmath>
 
 #include "controlador.h"
 #include "definicoes_sistema.h"
@@ -19,6 +20,7 @@ Controlador::Controlador(int pinStepX, int pinDirX, int pinStepY, int pinDirY, i
 
 void Controlador::iniciarControlador()
 {
+    calibrando = false;
     xTaskCreate(
         vTaskControlador,
         "Controlador",
@@ -32,13 +34,17 @@ void Controlador::iniciarControlador()
 void Controlador::taskControlar()
 {
     while(1) {
-        if ((pStepperX->distanceToGo() == 0) && (pStepperY->distanceToGo() == 0) && (pStepperZ->distanceToGo() == 0)) {
-            chegou = true;
+        if (!calibrando) {
+            if ((pStepperX->distanceToGo() == 0) && (pStepperY->distanceToGo() == 0) && (pStepperZ->distanceToGo() == 0)) {
+                chegou = true;
+            } else {
+                chegou = false;
+                pStepperX->runSpeedToPosition();
+                pStepperY->runSpeedToPosition();
+                pStepperZ->runSpeedToPosition();
+            }
         } else {
-            chegou = false;
-            pStepperX->runSpeedToPosition();
-            pStepperY->runSpeedToPosition();
-            pStepperZ->runSpeedToPosition();
+
         }
         vTaskDelay(CONTROLADOR_DELAY / portTICK_PERIOD_MS);
     }
@@ -46,7 +52,34 @@ void Controlador::taskControlar()
 
 void Controlador::enviarComando(int G, float X, float Y, float Z)
 {
+    int stepsX = (int) X / MM_POR_STEP_X;
+    int stepsY = (int) Y / MM_POR_STEP_Y;
+    int stepsZ = (int) Z / MM_POR_STEP_Z;
 
+    if (G == 0) {
+        pStepperX->moveTo(stepsX);
+        pStepperY->moveTo(stepsY);
+        pStepperZ->moveTo(stepsZ);
+
+        pStepperX->setSpeed(speed);
+        pStepperY->setSpeed(speed);
+        pStepperZ->setSpeed(speed);
+    } else if (G == 1) {
+        pStepperX->moveTo(stepsX);
+        pStepperY->moveTo(stepsY);
+        pStepperZ->moveTo(stepsZ);
+
+        // Velocidades para trajetoria reta XY
+        float dist = sqrt(pow(stepsX - pStepperX->currentPosition(),2) + 
+                          pow(stepsY - pStepperY->currentPosition(),2) 
+                         );
+        float speedX = speed * (stepsX - pStepperX->currentPosition()) / dist;
+        float speedY = speed * (stepsY - pStepperY->currentPosition()) / dist;
+
+        pStepperX->setSpeed(speedX);
+        pStepperY->setSpeed(speedY);
+        pStepperZ->setSpeed(speed);
+    }
 }
 
 
